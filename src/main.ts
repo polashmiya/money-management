bd40ace2f5d562158905b3de86dbc48d3d9a9960
Masbha Uddin <masbhauddinsifat@gmail.com>
2021-03-13 13:04:12 +0600

    :hammer: global prefix and Logger message added in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1');
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
+
+  Logger.log(`Server Started at ${port}`, 'NestApplication');
 }
 bootstrap();
