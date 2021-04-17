1e45424f24c55be7a56556ee28da67779cc31e70
Masbha Uddin <masbhauddinsifat@gmail.com>
2021-04-18 03:29:26 +0600

    :hammer: home module & component added

--- frontend/src/app/@core/app-routing.module.ts
+++ frontend/src/app/@core/app-routing.module.ts
@@ -1,17 +1,22 @@
+import { Page404Component } from './../@auth/page404/page404.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'authentication',
-    pathMatch: 'full',
+    loadChildren: () =>
+      import('../@module/home/home.module').then((m) => m.HomeModule),
   },
   {
     path: 'authentication',
     loadChildren: () =>
       import('../@auth/auth.module').then((m) => m.AuthModule),
     canActivate: [],
+  },
+  {
+    path: '**',
+    component: Page404Component,
   },
 ];
 
