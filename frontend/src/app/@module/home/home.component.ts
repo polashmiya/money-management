8a4b36e8c3c963006b4c6b7989ed3b536634f331
Masbha Uddin <masbhauddinsifat@gmail.com>
2021-05-09 17:12:26 +0600

    :hammer: calendar added

--- frontend/src/app/@module/home/home.component.ts
+++ frontend/src/app/@module/home/home.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  public date: Date = new Date();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onDateSelect(date: Date) {
+    this.date = date;
+    console.log(date);
   }
-
 }
--- frontend/src/app/@module/home/home.module.ts
+++ frontend/src/app/@module/home/home.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
-  ]
+    HomeRoutingModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
 })
-export class HomeModule { }
+export class HomeModule {}
